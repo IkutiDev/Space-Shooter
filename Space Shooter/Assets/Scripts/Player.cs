5fb8447201783d6f36b899c2c5666b19bf9f81c5
Ikuti <borgitek@gmail.com>
2019-10-24 20:58:04 +0200

    Added player bounds

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -18,5 +18,17 @@ public class Player : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right*horizontalInput*speed*Time.deltaTime);
         transform.Translate(Vector3.up*verticalInput*speed*Time.deltaTime);
+        if(transform.position.y>=6f){
+            transform.position=new Vector3(transform.position.x,6f,0);
+        }
+        else if(transform.position.y<=-4f){
+            transform.position=new Vector3(transform.position.x,-4f,0);
+        }
+        if(transform.position.x>11f){
+            transform.position=new Vector3(-11f,transform.position.y,0);
+        }
+        else if(transform.position.x<-11f){
+            transform.position = new Vector3(11f, transform.position.y,0);
+        }
     }
 }
