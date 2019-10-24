29b13298635ad05c5811356b8411c0e2d06c86f3
Ikuti <borgitek@gmail.com>
2019-10-24 20:38:12 +0200

    Added simple controls for the ship

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float speed = 3.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right*horizontalInput*speed*Time.deltaTime);
+        transform.Translate(Vector3.up*verticalInput*speed*Time.deltaTime);
     }
 }
